bb762bbecd3a5a5d521d1b838eef906a05981588
rednt <47644820+rednt@users.noreply.github.com>
2024-08-06 14:29:12 +0200

    paintComponent() is predefined apparently :)

--- src/pongGame.java
+++ src/pongGame.java
@@ -1,13 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class pongGame extends JPanel {
+public class pongGame extends JPanel{
 
     static final int Width = 640, Height = 480;
 
-    public void paintComps(Graphics g){
+    public void paintComponent(Graphics g){
         g.setColor(Color.BLACK);
         g.fillRect(0,0, Width,Height);
     }
 }
+
+
 
